5199f4f2cfac6ace888ea7778b66a01a2bc1a57d
Betintos <qed.beknazar@gmail.com>
2024-05-17 21:50:28 +0600

    changed server IP in ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["34.125.223.99", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["34.16.210.117", "localhost", "127.0.0.1"]
 
 AUTH_USER_MODEL = "accounts.User"
 
